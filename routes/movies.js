96ba1241d55290111f6a7d5cfd59d5109fd85f16
oben-desmond <obend678@gmail.com>
2023-09-19 19:57:50 +0100

    added the other movies

--- routes/movies.js
+++ routes/movies.js
@@ -13,43 +13,43 @@ const netflixMovies = [
     {
         posterURL: "https://www.liverpoolfilmoffice.tv/wp-content/uploads/2021/03/The-Irregulars-Netflix-Key-Art-Landscape-1024x629.jpg",
         movieURL: "",
-        title: "The Irregulars",
-        category: "Category 2",
+        title: "https://youtu.be/lTE5MAGpflw?si=PDwEQ2e0lpdWixUf",
+        category: "Super Hero",
         year: 2019
     },
     {
         posterURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRarp8CCDBNdUJeydcpv7cqucDxXCj9u-CodV4tmwBKT4o3p2YhN3Eu5a5eTcAv13eLVqQ&usqp=CAU",
-        movieURL: "",
-        title: "Movie 3",
-        category: "Category 1",
+        movieURL: "https://youtu.be/tqVVrTvrI8U?si=GjSa6u5J50yimviO",
+        title: "The Glory",
+        category: "Christian",
         year: 2021
     },
     {
         posterURL: "https://www.bt.com/content/dam/bt/portal/images/articles/tv/tv-film-netflix-grey-man-new-keyart.jpg/jcr:content/renditions/landscape-desktop.764.430.jpg",
         movieURL: "",
-        title: "Movie 4",
-        category: "Category 3",
+        title: "Gray Man",
+        category: "Science Fiction",
         year: 2022
     },
     {
         posterURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Duo0ktYYwZHHlPgOzq31rHsR3-MOPjWnEDmsZ48-nHEYN4Mpr8VpAN1jUWzpiE56WGY&usqp=CAU",
-        movieURL: "",
-        title: "Movie 5",
-        category: "Category 2",
+        movieURL: "https://youtu.be/BmllggGO4pM?si=DrLtG-WuWtuBKr60",
+        title: "Dark",
+        category: "Horror",
         year: 2020
     },
     {
         posterURL: "https://i.ytimg.com/vi/5oFOGQJ_91c/maxresdefault.jpg",
-        movieURL: "",
-        title: "Movie 6",
-        category: "Category 3",
+        movieURL: "https://youtu.be/IE8HIsIrq4o?si=mKFGQaO9zYcs-lK6",
+        title: "The Adam Project",
+        category: "Horror",
         year: 2021
     },
     {
         posterURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgmuhfXZKo1_dtYT6AjGlNRjRatoR9zaTnSw&usqp=CAU",
-        movieURL: "",
-        title: "Movie 7",
-        category: "Category 1",
+        movieURL: "https://youtu.be/OgW7L2uq2VM?si=16zjL8YnMxUWXfxB",
+        title: "Sanctum",
+        category: "fiction",
         year: 2023
     }
 ];
